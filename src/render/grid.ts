0ad90cbabd55ef946d0b667428db28f5c0c25de7
tourn <latzer.daniel@gmail.com>
2016-11-16 08:47:18 +0100

    Draw connections between unit tiles

--- src/render/grid.ts
+++ src/render/grid.ts
@@ -24,6 +24,10 @@ export class CanvasGrid{
     this.ctx.fillRect(xPos,yPos,tileSize,tileSize);
   }
 
+  protected topLeftPixel(coordinate: number){
+    return coordinate * (tileSize + borderSize) - (borderSize/2)
+  }
+
   //later enable sprites here
   public drawGlyph(coords: Coords, glyph: string){
     const xPos = coords.x * (tileSize + borderSize) - (borderSize/2)
@@ -37,10 +41,43 @@ export class CanvasGrid{
   }
 
   public drawUnit(coords: Coords[], color: string = 'blue', glyph: string = 'x'){
+    var prev: Coords = null;
     for(const c of coords){
       this.drawTile(c.x, c.y, color);
+      if(prev){
+        this.drawConnection(prev, c, color);
+      }
+      prev = c;
     }
     this.drawGlyph(coords[0], glyph);
+  }
+
+  protected drawConnection(from: Coords, to: Coords, color: string = 'blue'){
+    const xOffset = from.x - to.x;
+    const yOffset = from.y - to.y;
+
+    this.ctx.fillStyle = color;
+    if(xOffset == -1){ //draw to the right
+      const x = this.topLeftPixel(from.x) + tileSize;
+      const y = this.topLeftPixel(from.y) + tileSize/3;
+      this.ctx.fillRect(x,y,borderSize, tileSize/3);
+    } else if (xOffset == 1){ //left
+      const x = this.topLeftPixel(to.x) + tileSize;
+      const y = this.topLeftPixel(to.y) + tileSize/3;
+      this.ctx.fillRect(x,y,borderSize, tileSize/3);
+    } else if (yOffset == -1){ //bottom
+      const x = this.topLeftPixel(from.x) + tileSize/3;
+      const y = this.topLeftPixel(from.y) + tileSize;
+      this.ctx.fillRect(x,y, tileSize/3, borderSize);
+    } else if (yOffset == 1){ //top
+      const x = this.topLeftPixel(to.x) + tileSize/3;
+      const y = this.topLeftPixel(to.y) + tileSize;
+      this.ctx.fillRect(x,y, tileSize/3, borderSize);
+    } else {
+      console.log("Invalid connection from " + JSON.stringify(from) + " to " + JSON.stringify(to));
+    }
+
+
   }
 }
 
